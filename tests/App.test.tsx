1d590638e8858971b24b4ddf0b46050d2d6008cb
Asko Ohmann <asko.ohmann@helmes.com>
2025-01-23 13:11:40 +0200

    Use test instead of describe for App

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,4 +1,4 @@
-import {beforeEach, describe, expect, it, vi} from "vitest";
+import {expect, vi, test} from "vitest";
 import {render, screen, waitForElementToBeRemoved} from "@testing-library/react";
 import App from "../src/App";
 import {fetchPosts} from "../src/api/dummyjson";
@@ -6,15 +6,10 @@ import * as React from "react";
 
 vi.mock('../src/api/dummyjson.ts')
 
-describe('App', () => {
-    beforeEach(() => {
-        vi.mocked(fetchPosts).mockResolvedValue([])
-    })
+test('App renders correctly', async () => {
+    vi.mocked(fetchPosts).mockResolvedValue([])
+    render(<App />);
 
-    it('renders correctly', async () => {
-        render(<App />);
-
-        expect(screen.getByRole('heading', {name: /Awesome UI App/i})).toBeInTheDocument()
-        await waitForElementToBeRemoved(() => screen.getByTitle(/Loading/i))
-    })
+    expect(screen.getByRole('heading', {name: /Awesome UI App/i})).toBeInTheDocument()
+    await waitForElementToBeRemoved(() => screen.getByTitle(/Loading/i))
 })
